cb2d11105561a4df26d3a0542fe113883d295df3
GloriaY007 <glorrossy@gmail.com>
2021-05-04 16:50:48 -0400

    Adding query file and retirement info table

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,59 +1,57 @@
 -- Creating tables for PH-EmployeeDB
--- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
-     PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
 );
-CREATE TABLE employees (
-	     emp_no INT NOT NULL,
-     birth_date DATE NOT NULL,
-     first_name VARCHAR NOT NULL,
-     last_name VARCHAR NOT NULL,
-     gender VARCHAR NOT NULL,
-     hire_date DATE NOT NULL,
-	 to_date DATE NOT NULL,
-     PRIMARY KEY (emp_no)
+create table dept_emp (
+	emp_no int not null,
+	dept_no varchar not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no),
+	primary key (emp_no, dept_no)
 );
-CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+create table employees (
+	emp_no int not null,
+	birth_date date not null,
+	first_name varchar not null,
+	last_name varchar not null,
+	gender varchar not null,
+	hire_date date not null,
+	primary key (emp_no)
 );
-CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+create table managers (
+	dept_no varchar not null,
+	emp_no int not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no),
+	primary key (emp_no, dept_no)
 );
-CREATE TABLE dept_emp (
-  dept_no VARCHAR(4) NOT NULL,
-  emp_no INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no, emp_no)
+create table salaries (
+	emp_no int not null,
+	salary int not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	primary key (emp_no)
 );
-CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (title)
+create table titles (
+	emp_no int not null,
+	title varchar not null,
+	from_date date not null,
+	to_date date not null,
+	foreign key (emp_no) references employees (emp_no),
+	primary key (emp_no, from_date)
 );
 
-SELECT * FROM departments;
-SELECT * FROM employees;
-SELECT * FROM dept_manager;
-SELECT * FROM salaries;
-SELECT * FROM dept_emp;
-SELECT * FROM titles;
+select * from departments;
+select * from dept_emp;
+select * from employees;
+select * from managers;
+select * from salaries;
+select * from titles;
